dcea4414131f9b254ee5aa4f8125dd13ab0b0216
Pure <leejy137@gmail.com>
2015-05-04 13:51:55 +0900

    example bootstrap desing apply

--- jquery.bootstrap-pureBattery.js
+++ jquery.bootstrap-pureBattery.js
@@ -4,9 +4,9 @@
 
 (function($) {
 	var definInfo = {
-			dataKey: "pureBatteryOptions",
-			progressBarId: "pure-battery-progressbar",
-			levelValueId: "pure-battery-value-span",
+			dataKey: 'pureBatteryOptions',
+			progressBarId: 'pure-battery-progressbar',
+			levelValueId: 'pure-battery-value-span',
 			
 	}
 	
@@ -21,38 +21,44 @@
 		var level = Math.round(battery.level * 100);
 		var levelStep = level;
 		if(option.progressStep > 0){
+			// progressStep
 			levelStep = Math.floor(level / option.progressStep) * option.progressStep;
+			
+			// min width
+			if(levelStep < option.progressStep){
+				levelStep = option.progressStep / 2;
+			}
 		}
 		
-		var progressbar = element.find("#"+definInfo.progressBarId);
+		var progressbar = element.find('#'+definInfo.progressBarId);
 		
-		progressbar.removeClass("progress-bar-success progress-bar-warning progress-bar-danger progress-bar-info");
-		progressbar.find("#"+definInfo.levelValueId).remove();
+		progressbar.removeClass('progress-bar-success progress-bar-warning progress-bar-danger progress-bar-info');
+		progressbar.find('#'+definInfo.levelValueId).remove();
 		
-		var batteryValue = $("<span></span>")
-			.attr("id", definInfo.levelValueId)
-			.css({"color": option.fontColor});
+		var batteryValue = $('<span></span>')
+			.attr('id', definInfo.levelValueId)
+			.css({'color': option.fontColor});
 			
 			
 		if(charging){
-			progressbar.addClass("progress-bar-info");
-			progressbar.append(batteryValue.addClass("glyphicon " + option.chargingIcon));
+			progressbar.addClass('progress-bar-info');
+			progressbar.append(batteryValue.addClass('glyphicon ' + option.chargingIcon));
 		}else{
 			if(level > option.warningLevel){
-				progressbar.addClass("progress-bar-success");
+				progressbar.addClass('progress-bar-success');
 			}
 			else if(level > option.dangerLevel){
-				progressbar.addClass("progress-bar-warning");
+				progressbar.addClass('progress-bar-warning');
 			}
 			else{
-				progressbar.addClass("progress-bar-danger");
+				progressbar.addClass('progress-bar-danger');
 			}
 		}
 		
 		if(!charging || (charging && level < 100)){
-			progressbar.append(batteryValue.html(level + "%"));			
+			progressbar.append(batteryValue.html(level + '%'));			
 		}
-		progressbar.css({"width": levelStep + "%"});
+		progressbar.css({'width': levelStep + '%'});
 		
 		var data = 
 			{
@@ -71,32 +77,30 @@
 		
 		element.data(definInfo.dataKey, option);
 		
-		if(!element.prop("style").width){
-			element.css({"width": option.width});
+		if(!element.prop('style').width){
+			element.css({'width': option.width});
 		}
-		if(!element.prop("style").height){
-			element.css({"height": option.height});
+		if(!element.prop('style').height){
+			element.css({'height': option.height});
 		}
 		
-		var main = $("<div></div>")
-			.css({"width": "90%", "height": "100%", "display": "inline-block"})
+		var main = $('<div></div>')
+			.css({'width': '100%', 'height': '100%', 'display': 'inline-block'})
 			.appendTo(element);
 		
-		$('<div style="width: 2px; background-color: #333; height: 11px; margin-top: 5.5px; position: absolute; display: inline-block"></div>').appendTo(element);
-		
-		var progress = $("<div></div>")
-			.addClass("progress pure-battery-progress")
-			.css({"width": "100%", "height": "100%"})
-			.css({"background-color": option.backgroupColor, "border-color": option.borderColor})
+		var progress = $('<div></div>')
+			.addClass('progress pure-battery-progress')
+			.css({'width': '100%', 'height': '100%'})
+			.css({'background-color': option.backgroupColor, 'border-color': option.borderColor})
 			.appendTo(main);
 		
 		$('<div></div>')
-			.attr("id", definInfo.progressBarId)
-			.attr("role", "progressbar")
-			.attr("aria-valuemin", "0")
-			.attr("aria-valuemax", "100")
-			.addClass("progress-bar")
-			.css({"width": "100%"})
+			.attr('id', definInfo.progressBarId)
+			.attr('role', 'progressbar')
+			.attr('aria-valuemin', '0')
+			.attr('aria-valuemax', '100')
+			.addClass('progress-bar')
+			.css({'width': '100%'})
 			.appendTo(progress);
 	}
 	
@@ -109,8 +113,8 @@
 	
 	var defaultSettings = {
 			test: false,
-			width: "50px",
-			height: "22px",
+			width: '50px',
+			height: '22px',
 			fontColor: '#fff',
 			borderColor: '#333',
 			backgroupColor: '#555', 
@@ -119,7 +123,7 @@
 			dangerLevel: 30,
 			progressStep: 5,
 			dangerCallback: function(data){
-				console.log("pure battery. danger callback. battey.level: %d", data.level);
+				console.log('pure battery. danger callback. battey.level: %d', data.level);
 			},
 	};
 	
@@ -132,7 +136,7 @@
 		var element = $(this);
 		
 		if(!navigator.getBattery){
-			console.warn("not supported browser.");
+			console.warn('navigator.gerBattery API\'s not supported browser.');
 			return;
 		}
 
@@ -157,7 +161,7 @@
 		}
 		
 		if(element.data()[definInfo.dataKey]){
-			console.log("exist pure battry.");
+			console.log('exist pure battry.');
 			return $(this);
 		}
 		
