92354a262119c5f32cf0c8607d3ed4e48168b702
Pure <leejy137@gmail.com>
2015-05-06 16:00:21 +0900

    js 개선

--- jquery.bootstrap-pureBattery.js
+++ jquery.bootstrap-pureBattery.js
@@ -2,55 +2,131 @@
  * 
  */
 
-(function($) {
-	var definInfo = {
-			optionDataKey: 'pureBatteryOptions',
-			batteryDataKey: 'pureBatteryInfo',
-			progressBarId: 'pure-battery-progressbar',
-			levelValueId: 'pure-battery-value-span',
-			
++function($) {
+	'use strict';
+	
+	/**
+	 * Pure Battery Class Definition. 
+	 */
+	
+	var PureBattery = function(element, options){
+		this.options = options;
+		this.$element = element;
+		this.batteryInfo = null;
+		
+		this.makeBattery();
+		this.addBatteryEvent();
 	}
 	
-	var updateBatteryStatus = function(element, battery) {
-		var option = element.data()[definInfo.optionDataKey];
+	PureBattery.VERSION = '0.1-beta';
+	
+	PureBattery.DEFIN = {
+		progressBarId: 'pure-battery-progressbar',
+		levelValueId: 'pure-battery-value-span',
 		
-		if(!option){
-			return;
+		dangerCallbackEvent: 'battery-danger',
+		StableCallbackEvent: 'battery-stable',
+	}
+	
+	PureBattery.DEFAULTS = {
+		test: false,
+		width: '50px',
+		height: '22px',
+		fontColor: '#ffffff',
+		borderColor: '#333333',
+		backgroupColor: '#555555', 
+		chargingIcon: 'glyphicon-flash',
+		warningLevel: 50,
+		dangerLevel: 30,
+		progressStep: 5,
+	}
+	
+	PureBattery.prototype.makeBattery = function(){
+		var $element = this.$element;
+		var options = this.options;
+		
+		if(!$element.prop('style').width){
+			$element.css({'width': options.width});
 		}
+		if(!$element.prop('style').height){
+			$element.css({'height': options.height});
+		}
+		options.width = $element.prop('style').width;
+		options.height = $element.prop('style').height;
 		
-		element.data(definInfo.batteryDataKey, battery);
+		var main = $('<div></div>')
+		.css({'width': '100%', 'height': '100%', 'display': 'inline-block'})
+		.appendTo($element);
+		
+		var progress = $('<div></div>')
+		.addClass('progress pure-battery-progress')
+		.css({'width': '100%', 'height': '100%'})
+		.css({'background-color': options.backgroupColor, 'border-color': options.borderColor})
+		.appendTo(main);
+		
+		$('<div></div>')
+		.attr('id', PureBattery.DEFIN.progressBarId)
+		.attr('role', 'progressbar')
+		.attr('aria-valuemin', '0')
+		.attr('aria-valuemax', '100')
+		.addClass('progress-bar')
+		.css({'width': '100%'})
+		.appendTo(progress);
+	}
+	
+	PureBattery.prototype.updateBattery = function(){
+		var $element = this.$element;
+		var options = this.options;
+		
+		$element.css({'width': options.width});
+		$element.css({'height': options.height});
+
+		$element.find(".pure-battery-progress").css({'background-color': options.backgroupColor, 'border-color': options.borderColor});
+	}
+	
+	PureBattery.prototype.updateBatteryStatus = function(battery){
+		var $element = this.$element;
+		var options = this.options;
+
+		if(this.options.test){
+			battery = $.extend({}, this.batteryInfo, battery); 
+		}else if(!battery){
+			battery = this.batteryInfo;
+		}
+		this.batteryInfo = battery;
 		
 		var charging = battery.charging;
 		var level = Math.round(battery.level * 100);
 		var levelStep = level;
-		if(option.progressStep > 0){
+		if(options.progressStep > 0){
 			// progressStep
-			levelStep = Math.floor(level / option.progressStep) * option.progressStep;
+			levelStep = Math.floor(level / options.progressStep) * options.progressStep;
 			
 			// min width
-			if(levelStep < option.progressStep){
-				levelStep = option.progressStep / 2;
+			if(levelStep < options.progressStep){
+				levelStep = options.progressStep / 2;
 			}
 		}
+		this.batteryInfo.viewLevel = level;
 		
-		var progressbar = element.find('#'+definInfo.progressBarId);
+		var progressbar = $element.find('#'+PureBattery.DEFIN.progressBarId);
 		
 		progressbar.removeClass('progress-bar-success progress-bar-warning progress-bar-danger progress-bar-info');
-		progressbar.find('#'+definInfo.levelValueId).remove();
+		progressbar.find('#'+PureBattery.DEFIN.levelValueId).remove();
 		
 		var batteryValue = $('<span></span>')
-			.attr('id', definInfo.levelValueId)
-			.css({'color': option.fontColor});
+			.attr('id', PureBattery.DEFIN.levelValueId)
+			.css({'color': options.fontColor});
 			
 			
 		if(charging){
 			progressbar.addClass('progress-bar-info');
-			progressbar.append(batteryValue.addClass('glyphicon ' + option.chargingIcon));
+			progressbar.append(batteryValue.addClass('glyphicon ' + options.chargingIcon));
 		}else{
-			if(level > option.warningLevel){
+			if(level > options.warningLevel){
 				progressbar.addClass('progress-bar-success');
 			}
-			else if(level > option.dangerLevel){
+			else if(level > options.dangerLevel){
 				progressbar.addClass('progress-bar-warning');
 			}
 			else{
@@ -63,181 +139,124 @@
 		}
 		progressbar.css({'width': levelStep + '%'});
 		
-		var data = 
-			{
-				charging: charging, 
-				level: level, 
-				//chargingTime: battery.chargingTime, 
-				//dischargingTime: battery.dischargingTime
-			};
-		callDangerFunction(data, option);
-	}
-
-	var makeBattery = function(element, option){
-		if(!element){
-			return;
-		}
-		
-		element.data(definInfo.optionDataKey, option);
-		
-		if(!element.prop('style').width || option.options){
-			element.css({'width': option.width});
-		}
-		if(!element.prop('style').height || option.options){
-			element.css({'height': option.height});
-		}
-		option.width = element.prop('style').width;
-		option.height = element.prop('style').height;
-		
-		if(option.options){
-			element.find(".pure-battery-progress").css({'background-color': option.backgroupColor, 'border-color': option.borderColor});
-		}else{
-			var main = $('<div></div>')
-			.css({'width': '100%', 'height': '100%', 'display': 'inline-block'})
-			.appendTo(element);
-			
-			var progress = $('<div></div>')
-			.addClass('progress pure-battery-progress')
-			.css({'width': '100%', 'height': '100%'})
-			.css({'background-color': option.backgroupColor, 'border-color': option.borderColor})
-			.appendTo(main);
-			
-			$('<div></div>')
-			.attr('id', definInfo.progressBarId)
-			.attr('role', 'progressbar')
-			.attr('aria-valuemin', '0')
-			.attr('aria-valuemax', '100')
-			.addClass('progress-bar')
-			.css({'width': '100%'})
-			.appendTo(progress);
-		}
+		this.eventTrigger();
 	}
 	
-	var callDangerFunction = function(data, option){
-		if(!data.charging && data.level <= option.dangerLevel && 
-				option.dangerCallback && typeof(option.dangerCallback) === 'function'){
-			option.dangerCallback(data);
-		}
-	}
-	
-	var defaultSettings = {
-			test: false,
-			width: '50px',
-			height: '22px',
-			fontColor: '#fff',
-			borderColor: '#333',
-			backgroupColor: '#555', 
-			chargingIcon: 'glyphicon-flash',
-			warningLevel: 50,
-			dangerLevel: 30,
-			progressStep: 5,
-			dangerCallback: function(data){
-				console.log('pure battery. danger callback. battey.level: %d', data.level);
-			},
-	};
-	
-	var defaultOptions = {
-			charging: false,
-			level: 100
-	}
-	
-	$.fn.pureBattery = function(option, optionName, optionValue){
-		var element = $(this);
-		
-		if(!navigator.getBattery){
-			console.warn('navigator.gerBattery API\'s not supported browser.');
-			return;
-		}
-
-		if(option && typeof(option) != 'object'){
-			var batteryOption = element.data()[definInfo.optionDataKey];
-			
-			if(!batteryOption){
-				console.log("not found pure battery option.");
-				return $(this);
-			}
-			
-			if(option === 'option'){
-				if(!optionName && !optionValue){
-					return batteryOption;
-				}
-				
-				var userOptions;
-				if(typeof(optionName) === 'object'){
-					userOptions = $.extend({}, batteryOption, optionName);
-				}else{
-					if(!batteryOption[optionName]){
-						console.error("not option. " + optionName);
-						return $(this);
-					}
-					
-					if(!optionValue){
-						return batteryOption[optionName];
-					}
-					
-					batteryOption[optionName] = optionValue;
-					userOptions = batteryOption;
-				}
-				userOptions['options'] = true;
-				
-				makeBattery(element, userOptions);
-				
-				var battery = element.data()[definInfo.batteryDataKey];
-				if(battery){
-					updateBatteryStatus(element, battery);
-				}
-			}
-			
-			else if(batteryOption.test && option === 'test'){
-				if(typeof(optionName) === 'object'){
-					var userOptions = $.extend({}, defaultOptions, optionName);
-					
-					userOptions.level = userOptions.level / 100;
-					updateBatteryStatus(element, userOptions);
-				}else{
-					if(optionName === 'level'){
-						updateBatteryStatus(element, {charging: false, level: optionValue / 100});					
-					}else{
-						updateBatteryStatus(element, {charging: optionValue, level: 1});
-					}
-				}				
-			}
-			
-			return $(this);
-		}
-		
-		if(element.data()[definInfo.optionDataKey]){
-			console.log('exist pure battry.');
-			
-			
-			return $(this);
-		}
-		
-		var settings = $.extend({}, defaultSettings, option);
+	PureBattery.prototype.addBatteryEvent = function(){
+		var THIS = this;
+		var options = this.options;
 		
 		navigator.getBattery().then(function(battery) {
-			// 
-			makeBattery(element, settings);
+			THIS.batteryInfo = battery;
 			
-	  		// Update the battery status initially when the promise resolves ...
-	  		updateBatteryStatus(element, battery);
-
-	  		// .. and for any subsequent updates.
+			THIS.updateBatteryStatus(battery);
+			
 	  		battery.addEventListener('chargingchange', function() {
-	  			if(settings.test){
+	  			if(options.test){
 	  				return;
 	  			}
-	  			updateBatteryStatus(element, battery);
+	  			
+	  			THIS.updateBatteryStatus(battery);
 	  		}, false);
 
 	  		battery.addEventListener('levelchange', function() {
-	  			if(settings.test){
+	  			if(options.test){
 	  				return;
 	  			}
-	  			updateBatteryStatus(element, battery);
+	  			
+	  			THIS.updateBatteryStatus(battery);
 	  		}, false);
-	  	});		
+	  	});			
+	}
+	
+	PureBattery.prototype.eventTrigger = function(){
+		var options = this.options;
+		var battery = this.batteryInfo;
+
+		var event = null;
+		if(!battery.charging && battery.viewLevel <= options.dangerLevel){
+			event = $.Event(PureBattery.DEFIN.dangerCallbackEvent);
+		}
+		else{
+			event = $.Event(PureBattery.DEFIN.StableCallbackEvent);
+		}
+		event.battery = {
+				level: battery.viewLevel,
+				charging: battery.charging,
+				dangerLevel: options.dangerLevel
+			};
+		this.$element.trigger(event);
+	}
+	
+	
+	PureBattery.prototype.option = function(name, value){
+		if(!name && !value){
+			return this.options;
+		}
 		
-		return $(this);
-	};
-})($);
+		if(typeof name == 'object'){
+			$.extend(this.options, name);
+		}
+		else if(typeof name == 'string' && this.options[name] != undefined){
+			this.options[name] = value;
+		}
+		else{
+			console.warn('not supported option.', name, value);
+			return;
+		}
+		
+		this.updateBattery();
+		this.updateBatteryStatus();
+		
+		return;
+	}
+	
+	PureBattery.prototype.test = function(name, value){
+		if(!this.options.test){
+			console.log("disable test option. ", this.options.test);
+			return;
+		}
+		
+		var testObject = {};
+		
+		if(typeof name == 'object'){
+			testObject = name;
+		}
+		else if(typeof name == 'string' && this.batteryInfo[name] != undefined){
+			testObject[name] = value;
+		}
+		else{
+			console.warn('not supported option.', name, value);
+			return;			
+		}
+		
+		if(testObject.level) testObject.level = testObject.level / 100;
+		
+		this.updateBatteryStatus(testObject);
+		
+		return;
+	}
+	
+	/**
+	 * Pure Battery Plug-in Definition.
+	 */
+	
+	function Plugin(option, _relatedTarget, _relatedValue){
+		var $this = $(this);
+		
+		if(!navigator.getBattery){
+			console.warn('navigator.gerBattery API\'s not supported browser.');
+			return $this;
+		}
+		
+		var data    = $this.data('pure.pureBattery');
+		var options = $.extend({}, PureBattery.DEFAULTS, $this.data, typeof option == 'object' && option);
+
+		if(!data) $this.data('pure.pureBattery', (data = new PureBattery(this, options)));
+		if(typeof option == 'string') return data[option](_relatedTarget, _relatedValue);
+	}
+	
+	$.fn.pureBattery = Plugin;
+	$.fn.pureBattery.Constructor = PureBattery;
+	
+}(jQuery);
