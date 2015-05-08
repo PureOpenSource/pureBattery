86fbe9f9d3c1066ff78a04bd58283a79b5a60393
Pure <leejy137@gmail.com>
2015-05-08 17:28:37 +0900

    소스 정리

--- example/example.js
+++ example/example.js
@@ -25,45 +25,7 @@ $(document).ready(function(){
 		example.customOptionTest();
 	});
 	
-	//////////////////////////////////////////////////////////////
-	
-	$("#customWidthValue").on("change", function(){
-		var width = $(this).val();
-		example.changeWidth(width);
-	})
-	$("#customWidth").on("click", function(){
-		var width = $("#customWidthValue").val();
-		example.changeWidth(width);
-	});
-	$("#defaultWidth").on("click", function(){
-		$("#customWidthValue").val(50).change();
-	})
-	
-	$("#moveBtn").on("click", function(){
-		if(example.info.isStart){
-			$(this).button("loading", "Stoping...").button('loading')
-				.data("resetText", "Start");
-			
-			clearInterval(example.info.procTest);
-			clearInterval(example.info.procRandom);
-		}else{
-			$(this).button("loading", "Starting...").button('loading')
-				.data("resetText", "Stop");
-			example.startProcess();
-		}
-		
-		$(this).button('reset');
-		
-		example.info.isStart = !example.info.isStart;
-	});
-	
 	///////////////////////////////////////////////////////////
-	$("#pureBattery-default").pureBattery();
-	$("#pureBattery-custom_wh").pureBattery();
-	$("#pureBattery-custom_o_wh").pureBattery({
-		width: "100px",
-		height: "30px"
-	});
 	$("#pureBattary-custom_options").pureBattery();
 	$("#pureBattary-custom_options").on("battery-danger", function(e){
 		var battery = e.battery;
@@ -88,28 +50,10 @@ $(document).ready(function(){
 		$("#customOptionDanger").addClass('alert-success').html(html);
 	});
 	
-	$("#pureBattery-not_charging-random").pureBattery({test: true});
-	$("#pureBattery-charging-random").pureBattery({test: true});
-	$("#pureBattery-not_charging-minus").pureBattery({test: true});
-	$("#pureBattery-charging-plus").pureBattery({test: true});
-	
 	example.init();
 });
 
 var example = {
-		info: {
-			isStart: true,
-			procTest: null,
-			procRandom: null,
-		},
-		
-		init: function(){
-			$("#pureBattery-not_charging-minus").data("battery_level", 100);
-			$("#pureBattery-charging-plus").data("battery_level", 0);
-			
-			setTimeout(example.startProcess, 1000);				
-		},
-		
 		customOption: function(inputElement, isDefalut){
 			var optionName = inputElement.data("option");
 			var optionValue = inputElement.val();
@@ -134,39 +78,5 @@ var example = {
 			$levelPrint.html(level);
 			
 			$("#pureBattary-custom_options").pureBattery('test', {charging: charging, level: level});
-		},
-		
-		randomLevel: function(){
-			$("#pureBattery-not_charging-random").pureBattery("test", {"charging": false, "level": (Math.random() * 100) + 1});
-			$("#pureBattery-charging-random").pureBattery("test", {"charging": true, "level": (Math.random() * 100) + 1});
-		},
-		
-		testLevel: function(){
-			var levelMinus = $("#pureBattery-not_charging-minus").data("battery_level");
-			var levelPlus = $("#pureBattery-charging-plus").data("battery_level");
-			
-			$("#pureBattery-not_charging-minus").pureBattery("test", {"charging": false, "level": (levelMinus--)});
-			$("#pureBattery-charging-plus").pureBattery("test", {"charging": true, "level": (levelPlus++)});
-			
-			if(levelMinus < 0){
-				levelMinus = 100;
-			}
-			if(levelPlus > 100){
-				levelPlus = 0;
-			}
-			
-			$("#pureBattery-not_charging-minus").data("battery_level", levelMinus);
-			$("#pureBattery-charging-plus").data("battery_level", levelPlus);				
-		},			
-		
-		startProcess: function(){
-			example.info.procTest = setInterval(example.testLevel, 150);
-			
-			example.randomLevel();
-			example.info.procRandom = setInterval(example.randomLevel, 3000);				
-		},
-		
-		changeWidth: function(width){
-			$(".change-width").css({"width": width+"px"});
 		},
 }
